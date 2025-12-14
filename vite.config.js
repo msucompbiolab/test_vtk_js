104debc486e709c4a50f18c9f4f341b7abdbe5bf
likchuan <lclee@egr.msu.edu>
2025-12-14 18:43:19 -0500

    Add GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/test_vtk_js/',  // Match your repository name
   plugins: [
     vue(),
     vueDevTools(),
